b4fec04e6ad461aad222f3535ef5356b7a5b51e6
Cornel Goran <cornelg7@yahoo.com>
2018-10-28 21:39:53 +0200

    Added game logic

---
+++ src/main/scala/beautycontest/Controlers/Game.scala
@@ -0,0 +1,11 @@
+package beautycontest.Controlers
+
+class Game(numberOfPlayers: Integer, numberOfRounds: Integer, playerList: List[Player]) {
+
+  var previousRoundList:List[Round] = List[Round]()
+
+  var currentRoundNumber = 0
+
+
+
+}
