1037547dec92225f2a316612bf00ab287c26e2ed
Cornel Goran <cornelg7@yahoo.com>
2018-10-30 14:15:21 +0200

    Overridden equals method

--- src/main/scala/beautycontest/Controlers/Player.scala
+++ src/main/scala/beautycontest/Controlers/Player.scala
@@ -4,4 +4,9 @@ trait Player {
   var uid: Int = 0
   def name: String
   def getChoice: Int
+
+  override def equals(obj: Any): Boolean = obj match {
+    case x: Player => x.uid == this.uid
+    case _ => super.equals(obj)
+  }
 }
